745e43daa16f8bc57bf7b39b1bac80bcc11b399b
SekiAsuka <a.seki.sys24@morijyobi.ac.jp>
2025-06-09 14:53:02 +0900

    データベース

--- config/settings.py
+++ config/settings.py
@@ -88,7 +88,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-   "default": config("DATABASE_URL", default=default_dburl, cast=dburl),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
